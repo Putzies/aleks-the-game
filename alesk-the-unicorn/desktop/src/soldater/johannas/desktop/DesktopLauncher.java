f833c2342a7209f44d177731fae19d3f587da6c8
Pontus Stjernström <pontus@stjernstrom.nu>
2018-03-20 10:04:55 +0100

    Added libgdx boilerplate!

---
+++ alesk-the-unicorn/desktop/src/soldater/johannas/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package soldater.johannas.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import soldater.johannas.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
