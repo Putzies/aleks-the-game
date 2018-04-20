b48c20241818b446a7efacfe8962307dd4f71420
jtorbjornsson <jt@johannatorbjornsson.se>
2018-04-20 14:01:20 +0200

    new imagas

--- alesk-the-unicorn/core/src/soldater/johannas/model/Baguette.java
+++ alesk-the-unicorn/core/src/soldater/johannas/model/Baguette.java
@@ -3,8 +3,8 @@ package soldater.johannas.model;
 public class Baguette extends Pickup {
 
     // TODO Remember to double check the actual size of the Lunchboxes!
-    public static final int WIDTH = 50;
-    public static final int HEIGHT = 50;
+    public static final int WIDTH = 51;
+    public static final int HEIGHT = 39;
 
     @Override
     public int getWidth() {
