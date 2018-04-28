d907aa8099953a603ea5dea63f3075670da6d832
jtorbjornsson <jt@johannatorbjornsson.se>
2018-04-28 19:22:12 +0200

    added some comments

--- alesk-the-unicorn/core/src/soldater/johannas/view/RainbowEmitter.java
+++ alesk-the-unicorn/core/src/soldater/johannas/view/RainbowEmitter.java
@@ -13,7 +13,7 @@ public class RainbowEmitter {
     private List<RainbowParticle> rainbow;
     private Texture texture = new Texture("rainbow2.png");
     private int width = 3;
-    private int nrXSprites;
+    private int roundedTipSize;
     private double x;
     private double y;
     private double xVel;
@@ -22,13 +22,18 @@ public class RainbowEmitter {
 
     public RainbowEmitter(){
         rainbow = new ArrayList<RainbowParticle>();
-        nrXSprites = texture.getWidth()/width;
-
+        roundedTipSize = texture.getWidth()/width;
     }
 
+    // Add new rainbow particles after the unicorn and delete them after a while
     public void update(double dTime, double x, double y, boolean add) {
+
+        // Make the rainbow start at the middle of the unicorn
         y += 15;
+
+        // Since the width of the rainbow particle sprite is 3, xVel has to be rounded to a multiple of three, to make it look nice
         xVel = roundTo3(x - this.x);
+
         yVel = (y - this.y);
         this.x = x;
         this.y = y;
@@ -38,6 +43,7 @@ public class RainbowEmitter {
         deleteParticles(dTime);
     }
 
+    // Add particles at the current location and in the gap between current and last position
     private void addParticles(double x, double y) {
             int ratio = (int)Math.abs(xVel)/width;
             for(double i = width; i <= xVel; i += width) {
@@ -48,6 +54,7 @@ public class RainbowEmitter {
             }
     }
 
+    // Remove particles when it is time for them to be deleted
     private void deleteParticles(double dTime) {
         for (Iterator<RainbowParticle> iterator = rainbow.iterator(); iterator.hasNext();) {
             RainbowParticle particle = iterator.next();
@@ -58,19 +65,23 @@ public class RainbowEmitter {
         }
     }
 
+    // Draw the rainbow
     public void draw(SpriteBatch batch, int xOffset, int yOffset) {
             for (int i = 0; i< rainbow.size(); i++) {
                 RainbowParticle rp = rainbow.get(i);
-                if(rainbow.size() - i < nrXSprites) {
+
+                // Draw a rounded tip at the beginning of the rainbow
+                if(rainbow.size() - i < roundedTipSize) {
                     batch.draw(
                             texture,
                             (int)(rp.getX() + xOffset),
                             (int)(rp.getY() + yOffset),
-                            Math.abs((rainbow.size() - i)-nrXSprites)*rp.getWidth(),
+                            Math.abs((rainbow.size() - i)-roundedTipSize)*rp.getWidth(),
                             rp.getFadeLevel() * rp.getHeight(),
                             rp.getWidth(),
                             rp.getHeight()
                     );
+                // Draw the rest of the rainbow
                 } else {
                     batch.draw(
                             texture,
