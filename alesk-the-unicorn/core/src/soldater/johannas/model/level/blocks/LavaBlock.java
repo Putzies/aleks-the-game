6718b4bdc52cb5cfd74c3abad1e1bd12403bdc43
jtorbjornsson <jt@johannatorbjornsson.se>
2018-05-22 15:25:52 +0200

    Fixed block bug

--- alesk-the-unicorn/core/src/soldater/johannas/model/level/blocks/LavaBlock.java
+++ alesk-the-unicorn/core/src/soldater/johannas/model/level/blocks/LavaBlock.java
@@ -6,11 +6,7 @@ public class LavaBlock extends Block {
     public LavaBlock(int x, int y) {
         super(x, y);
     }
-
-
-    public boolean isHarmful() {
-        return true;
-    }
+    
     @Override
     public String getName() {
         return "lavaBlock";
